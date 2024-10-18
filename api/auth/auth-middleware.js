6d6c8d61a6e6a7f708b3305a851fe306b5c9bd46
Bethany Ingraham <BethFIngraham@gmail.com>
2024-10-18 19:15:03 -0400

    validateRoleName middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -65,7 +65,25 @@ const validateRoleName = (req, res, next) => {
       "message": "Role name can not be longer than 32 chars"
     }
   */
- next()
+  const {role_name} = req.body;
+
+  if(role_name === undefined || role_name.trim() === '') {
+    req.role_name = 'student';
+    next();
+  } else if(role_name === 'admin') {
+    next({
+      status: 422,
+      message: 'Role name can not be admin'
+    });
+  } else if(role_name.trim().length > 32) {
+    next({
+      status: 422,
+      message: 'Role name can not be longer than 32 chars'
+    });
+  } else {
+    req.role_name = role_name.trim();
+    next();
+  }
 }
 
 module.exports = {
